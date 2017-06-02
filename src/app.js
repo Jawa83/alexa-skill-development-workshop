a401afa8f76f6b3431430eab6697543866f49850
Ward Janssens <ward.janssens2@kbc.be>
2017-06-02 15:25:56 +0200

    update

--- src/app.js
+++ src/app.js
@@ -57,10 +57,11 @@ module.exports.guessHandlers = Alexa.CreateStateHandler(states.GUESS, {
             this.emit(':ask', 'The number is too low', 'The number is too low');
         } else if (guessNum === targetNum){
             // With a callback, use the arrow function to preserve the correct 'this' context
-            this.emit(':ask', guessNum.toString() + 'is correct! Would you like to play a new game?',
+            this.emit(':ask', guessNum.toString() + ' is correct! Would you like to play a new game?',
                 'Say yes to start a new game, or no to end the game.');
         } else {
             this.emit(':ask', 'That is not a number, try again');
+            this.handler.state = states.START;
         }
     },
     Unhandled() {
