4a7d4c44724820c694c2bc668bb9a06c27c48b9c
Ward Janssens <ward.janssens2@kbc.be>
2017-06-02 14:20:10 +0200

    update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,42 @@
-const WELCOME_MESSAGE = 'Hello Ward!';
+const Alexa = require('alexa-sdk');
+
+const WELCOME_MESSAGE = 'Welcome to High Low guessing game. Would you like to play?';
+const START_MESSAGE = 'Great! Try saying a number to start the game.';
+const EXIT_SKILL_MESSAGE = 'Bye Bye';
+const HELP_MESSAGE = 'Say a number.';
+
+const states = {
+    START: '_START'
+};
 
 module.exports.handlers = {
     LaunchRequest() {
-        this.emit(':tell', WELCOME_MESSAGE);
+        this.handler.state = states.START;
+        this.emitWithState('Start');
+    },
+    Unhandled() {
+        this.handler.state = states.START;
+        this.emitWithState('Start');
     }
 };
+
+module.exports.startHandlers = Alexa.CreateStateHandler(states.START, {
+    Start() {
+        this.emit(':ask', WELCOME_MESSAGE, HELP_MESSAGE);
+    },
+    'AMAZON.YesIntent': function () {
+        this.emit(':ask, START_MESSAGE', START_MESSAGE);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', EXIT_SKILL_MESSAGE);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', EXIT_SKILL_MESSAGE);
+    },
+    'AMAZON.HelpIntent': function () {
+        this.emit(':ask', HELP_MESSAGE, HELP_MESSAGE);
+    },
+    Unhandled() {
+        this.emitWithState('Start');
+    }
+});
